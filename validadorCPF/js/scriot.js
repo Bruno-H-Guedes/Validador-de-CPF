5dd84ffdec1e61dd935d42bae13a20daa5759164
Bruno-H-Guedes <bruno.guedes@aluno.ufop.edu.br>
2023-06-16 11:57:17 -0300

    ValidadorCPF

---
+++ validadorCPF/js/scriot.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+    $('#cpf').inputmask('999.999.999-99');
+  });
+  
+  function validarCPF() {
+    const cpfFormatado = document.getElementById('cpf').value;
+    const cpf = limpaFormatacao(cpfFormatado);
+  
+    if (cpf.length !== 11) {
+      mostraResultado('CPF deve conter 11 digitos.', 'red');
+      return;
+    }
+  
+    if (verificaDigitosRepetidos(cpf)) {
+      mostraResultado('CPF nao pode ter todos os numeros repetidos.', 'red');
+      return;
+    }
+  
+    const digito1 = calculaDigitoVerificador(cpf, 1);
+    const digito2 = calculaDigitoVerificador(cpf, 2);
+  
+    if (!digito1) {
+      mostraResultado(`CPF invalido - ${cpfFormatado}`, 'red');
+      return;
+    }
+  
+    if (!digito2) {
+      mostraResultado(`CPF invalido - ${cpfFormatado}`, 'red');
+      return;
+    }
+    else{
+    mostraResultado(`CPF válido - ${cpfFormatado}`, 'green');
+    }
+  }
+  
+  function calculaDigitoVerificador(cpf, posicao) {
+    const sequencia = cpf.slice(0, 8 + posicao).split('');
+    let soma = 0;
+    let multiplicador = 9 + posicao;
+  
+    for (const numero of sequencia) {
+      soma += multiplicador * Number(numero);
+      multiplicador--;
+    }
+  
+    const restoDivisao = (soma * 10) % 11;
+    const digito = cpf.slice(8 + posicao, 9 + posicao);
+  
+    return restoDivisao == digito;
+  }
+  
+  function limpaFormatacao(cpf) {
+    cpf = cpf.replace(/\D/g, '');
+    return cpf;
+  }
+  
+  function mostraResultado(texto, cor) {
+    const span = document.getElementById('resultado');
+    span.innerHTML = texto;
+    span.style.color = cor;
+  }
+  
+  function verificaDigitosRepetidos(cpf) {
+    return cpf.split('').every((d) => d === cpf[0]);
+  }
+  
